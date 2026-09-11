acd6c7ccbb6897e588804318c87c580312d4a4b0
huynhhienanhquy2005 <quyhuynh.tp2005@email.com>
2026-09-11 15:40:21 +0700

    tools/index.ts: export all tool functions (calculator, getCurrentTime, openUrl, updateTaskState)

---
+++ practices/ag-ui-custom-agent/src/tools/index.ts
@@ -0,0 +1,8 @@
+export { calculator } from "./calculator.js";
+export { getCurrentTime } from "./current-time.js";
+export { openUrl } from "./open-url.js";
+
+export {
+  updateTaskState,
+  type TaskState,
+} from "./update-task-state.js";
